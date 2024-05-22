1c24b540ed445546f980d8d289d6d133dcc262cc
Daniele Bortoluzzi <daniele.bortoluzzi@unito.it>
2024-05-22 13:03:57 +0200

    reduce number of robots to 3

--- PoC/AP_Engine/ap_run.sh
+++ PoC/AP_Engine/ap_run.sh
@@ -10,4 +10,4 @@ fi
 echo "Compiling target ap_engine..."
 
 # Run AP with simulator
-AP_ROUND_PERIOD=0.5 AP_COMM_RANGE=100 AP_ROBOT_COUNT=5 ./make.sh gui run -O -DAP_ENGINE_DEBUG=true ap_engine
+AP_ROUND_PERIOD=0.5 AP_COMM_RANGE=100 AP_ROBOT_COUNT=3 ./make.sh gui run -O -DAP_ENGINE_DEBUG=true ap_engine
